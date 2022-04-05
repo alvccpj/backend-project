d3707da892e5af7f3b82384ac711d5d222f6c780
Álvaro Jordão <alvccpj@gmail.com>
2022-04-05 19:49:21 -0300

    Add Initial SpringBoot Web project files

---
+++ src/main/java/com/project/professorallocation/ProfessorAllocationAlvaroApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationAlvaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationAlvaroApplication.class, args);
+	}
+
+}
