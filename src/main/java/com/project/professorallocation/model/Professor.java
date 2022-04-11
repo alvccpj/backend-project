cce387131f18c0003a37318690b6992cb4bc3e64
Álvaro Jordão <alvccpj@gmail.com>
2022-04-11 19:57:15 -0300

    Add Entity Classes

---
+++ src/main/java/com/project/professorallocation/model/Professor.java
@@ -0,0 +1,73 @@
+package com.project.professorallocation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Professor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(length = 11, nullable = false)
+	private String cpf;
+	
+	@Column(nullable = false)
+	private Long departmentId;
+	
+
+	public Professor() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	@Override
+	public String toString() {
+		return "Professor [id=" + id + ", name=" + name + ", cpf=" + cpf + ", departmentId=" + departmentId + "]";
+	}
+	
+	
+	
+}
+
+
+
+
